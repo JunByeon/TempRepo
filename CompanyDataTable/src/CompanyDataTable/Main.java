b29a309fc992be0d0cc25c9300dcbcb9d7bbd348
JunByeon <143871011+JunByeon@users.noreply.github.com>
2023-10-27 15:14:29 +0900

    Add files via upload

---
+++ CompanyDataTable/src/CompanyDataTable/Main.java
@@ -0,0 +1,9 @@
+package CompanyDataTable;
+
+import CompanyDataTable.View.ViewTable;
+
+public class Main {
+    public static void main(String[] args) {
+        new ViewTable();
+    }
+}
---
+++ CompanyDataTable/src/CompanyDataTable/ViewController/ViewController.java
@@ -0,0 +1,7 @@
+package CompanyDataTable.ViewController;
+
+import java.awt.event.ActionEvent;
+
+public interface ViewController {
+    public void actionPerformed(ActionEvent e);
+}
